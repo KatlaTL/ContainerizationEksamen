56e4b774b41c487dcb69fbfe26354f0c8fac9646
Asger Thorsboe Lundblad <balloupjuske2208@gmail.com>
2024-05-10 11:26:22 +0200

    changed config and gitignore

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,8 +1,8 @@
 module.exports = {
   username: process.env.DB_USERNAME || "root",
   password: process.env.DB_PASSWORD || "root",
-  database: process.env.DB_DATABASE || "zay",
-  //host: process.env.DB_HOSTNAME || "localhost",
-  host: 'db',
+  database: process.env.DB_DATABASE || "containerization",
+  host: process.env.DB_HOSTNAME || "localhost",
+  port: 3306,
   dialect: "mysql"
 }
