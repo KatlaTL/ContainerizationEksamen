e8846a81ac0e5faac4413032724de6689003ca28
Asger <regsa13@gmail.com>
2024-05-16 11:49:00 +0200

    created Dockerfile for backend and frontend. created docker-compose to run all nesecarry images at once

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,18 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [vue()],
+  preview: {
+    port: 8080,
+    strictPort: true,
+  },
+  server: {
+    port: 8080,
+    strictPort: true,
+    host: true,
+    origin: "http://0.0.0.0:8080",
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
